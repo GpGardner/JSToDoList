25662269e1b6a8611f83aa4d25014272d99f8a06
George Gardner <gpgardner@yahoo.com>
2020-04-06 19:58:09 -0400

    general refactoring

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const textInput = document.querySelector("#todoInput");
 function deletableItem() {
   document.querySelectorAll("li").forEach((item) => {
     item.addEventListener("click", () => {
-      item.classList.add("strikeout");
+      item.setAttribute('class', 'strikeout text-blur-out');
       setTimeout(() => {
         item.remove();
       }, 2000);
@@ -16,6 +16,7 @@ const submitBtn = document.querySelector("#submitBtn");
 submitBtn.addEventListener("click", function create(e) {
   const newListItem = document.createElement("li");
   newListItem.innerText = textInput.value;
+  newListItem.setAttribute('class', 'text-focus-in');
   list.appendChild(newListItem);
   textInput.value = "";
   deletableItem();
@@ -25,6 +26,7 @@ textInput.addEventListener("keydown", function (e) {
   if (e.keyCode === 13) {
     const newListItem = document.createElement("li");
     newListItem.innerText = textInput.value;
+    newListItem.setAttribute('class', 'text-focus-in');
     list.appendChild(newListItem);
     textInput.value = "";
     deletableItem();
