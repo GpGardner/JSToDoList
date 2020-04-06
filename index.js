eeebc3230df1de1a8f4fcffdcbcde1be0fa8d8e9
George Gardner <gpgardner@yahoo.com>
2020-04-06 16:04:21 -0400

    Made it possible to submit with enter

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-var list = document.querySelector(".list");
-var textInput = document.querySelector("#todoInput");
+const list = document.querySelector(".list");
+const textInput = document.querySelector("#todoInput");
 
-document.querySelectorAll("li").forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.add("strikeout");
-    setTimeout(() => {
-      item.remove();
-    }, 2000);
+function deletableItem() {
+  document.querySelectorAll("li").forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.add("strikeout");
+      setTimeout(() => {
+        item.remove();
+      }, 2000);
+    });
   });
-});
+}
 
-var submitBtn = document.querySelector("#submitBtn");
-submitBtn.addEventListener("click", function (e) {
-  var newListItem = document.createElement("li");
+const submitBtn = document.querySelector("#submitBtn");
+submitBtn.addEventListener("click", function create(e) {
+  const newListItem = document.createElement("li");
   newListItem.innerText = textInput.value;
   list.appendChild(newListItem);
   textInput.value = "";
+  deletableItem();
+});
+
+textInput.addEventListener("keydown", function (e) {
+  if (e.keyCode === 13) {
+    const newListItem = document.createElement("li");
+    newListItem.innerText = textInput.value;
+    list.appendChild(newListItem);
+    textInput.value = "";
+    deletableItem();
+  }
 });
