8255d4a390692ab1adc2ce8425911aab84fa127d
George Gardner <gpgardner@yahoo.com>
2020-04-09 19:18:28 -0400

    Adjusted click function

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 $(document).ready(deletableItem());
 
 function deletableItem() {
-  $('li').click(function () {
-    $(this).addClass('strikeout text-blur-out');
+  $("li").click(function () {
+    $(this).addClass("strikeout text-blur-out");
     setTimeout(() => {
       $(this).remove();
     }, 1250);
   });
-};
+}
 
-$('#submitBtn').click(function(){
+$("#submitBtn").click(function () {
   newItem();
 });
 
-$('#todoInput').keydown(function(event){
-  if(event.which == 13){
+$("#todoInput").keydown(function (event) {
+  if (event.which == 13) {
     newItem();
   }
 });
 
-function newItem (){
-  const adding = $('#todoInput').val();
-  $('.list').append('<li>'+adding+'</li>');
-  $('#todoInput').val('');
-  deletableItem()
+function newItem() {
+  const adding = $("#todoInput").val();
+  $(".list").append("<li>" + adding + "</li>");
+  $("#todoInput").val("");
+  deletableItem();
 }
